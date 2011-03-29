6d36a19348094265283a08ef28be02a036f6db97
Justin Morris <justin@pixelbloom.com>
2011-03-29 12:22:19 +1100

    WebGL viewport

--- coffeescript/main.coffee
+++ coffeescript/main.coffee
@@ -2,8 +2,8 @@ class Engine
     constructor: ->
         this.drawables = []
         this.remotes = new Remotes(this)
-        this.viewport = new CanvasViewport(this)
-        # this.viewport = new WebGLViewport(this)
+        # this.viewport = new CanvasViewport(this)
+        this.viewport = new WebGLViewport(this)
         this.physics = new Worker('/static/scripts/physics.js')
         this.physics.onmessage = this.physicsMessage
         this.physics.onerror = this.physicsError
@@ -55,8 +55,8 @@ class Remotes
 class Viewport
     constructor: (@parent) ->
         this.canvas = $('canvas#viewport')
-        this.context = this.canvas[0].getContext("2d")
         this.viewportScale = 20
+        this.fpsTarget = 1000/50
         this.width = 0
         this.height = 0
         this.loopTimer = false
@@ -65,7 +65,6 @@ class Viewport
         this.frames = 0
         this.lastUpdate = 0
         this.fps()
-        this.loop()
     
     loop: =>
         now = new Date().getTime()
@@ -73,7 +72,7 @@ class Viewport
         @lastUpdate = now
         this.draw()
         @frames++
-        @loopTimer = setTimeout(( => @loop() ), 1000/50)
+        @loopTimer = setTimeout(( => @loop() ), @fpsTarget)
     
     scale: (qty) =>
         qty * @viewportScale
@@ -86,6 +85,11 @@ class Viewport
 
 
 class CanvasViewport extends Viewport
+    constructor: (@parent) ->
+        super(@parent)
+        this.context = this.canvas[0].getContext("2d")
+        this.loop()
+    
     draw: =>
         @context.clearRect(0, 0, @canvas.width(), @canvas.height())
         for drawable in @parent.drawables
@@ -113,8 +117,166 @@ class CanvasViewport extends Viewport
 
 
 class WebGLViewport extends Viewport
-    draw: =>
+    constructor: (@parent) ->
+        super(@parent)
+        this.gl = WebGLUtils.create3DContext(@canvas[0], null)
+        this.mvMatrix = mat3.create()
+        this.pMatrix = mat3.create()
+        this.circleDetail = 16
+        this.circleEdges = 32
+        this.circleShapeVbo = this.createCircleShapeVbo()
+        this.boxShapeVbo = this.createBoxShapeVbo()
+        this.initShaders()
+        this.initDraw()
+        this.reshapeViewport()
+        this.loop()
+    
+    createStaticShapeVbo: (vertices, itemSize, numItems) =>
+        vbo = @gl.createBuffer()
+        @gl.bindBuffer(@gl.ARRAY_BUFFER, vbo)
+        @gl.bufferData(@gl.ARRAY_BUFFER, new Float32Array(vertices), @gl.STATIC_DRAW)
+        return {
+            numItems: numItems,
+            bind: (vertexPositionAttribute) =>
+                @gl.bindBuffer(@gl.ARRAY_BUFFER, vbo)
+                @gl.vertexAttribPointer(vertexPositionAttribute, itemSize, @gl.FLOAT, false, 0, 0)
+        }
+    
+    createBoxShapeVbo: =>
+        vertices = [-1.0, -1.0, -1.0,  1.0, 1.0,  1.0, 1.0, -1.0, -1.0, -1.0]
+        vbo = this.createStaticShapeVbo(vertices, 2, 5)
+        return {
+            bind: vbo.bind
+            fill: =>
+                @gl.drawArrays(@gl.TRIANGLE_FAN, 0, 4)
+            stroke: =>
+                @gl.drawArrays(@gl.LINE_STRIP, 0, 5)
+        }
+    
+    createCircleShapeVbo: =>
+        vertices = [0.0, 0.0]
+        step = 2.0 * Math.PI / @circleEdges
+        for i in [0...@circleEdges]
+            angle = step * i
+            vertices[i*2 + 2] = Math.cos(angle)
+            vertices[i*2 + 3] = Math.sin(angle)
+        vertices[@circleEdges*2 + 2] = vertices[2]
+        vertices[@circleEdges*2 + 3] = vertices[3]
+        vbo = this.createStaticShapeVbo(vertices, 2, @circleEdges+2)
+        return {
+            bind: vbo.bind
+            fill: =>
+                 @gl.drawArrays(@gl.TRIANGLE_FAN, 0, @circleEdges+2)
+            stroke: =>
+                @gl.drawArrays(@gl.LINE_STRIP, 1, @circleEdges+1)
+            destroy: =>
+                try
+                    @gl.deleteBuffer(vbo)
+                catch error
+                    # get a TypeError here in Chrome, hopefully the buffer will be destroyed by garbage collection
+                    console.log error
+        }
+    
+    setPMatrixUniform: =>
+        @gl.uniformMatrix3fv(@shaderProgram.pMatrixUniform, false, @pMatrix)
         
+    setMVMatrixUniform: =>
+        @gl.uniformMatrix3fv(@shaderProgram.mvMatrixUniform, false, @mvMatrix)
+    
+    initShaders: =>
+        vertex_shader_src = [
+            'attribute vec3 aVertexPosition;',
+            'uniform mat3 uMVMatrix;',
+            'uniform mat3 uPMatrix;',
+            'void main(void) {',
+            '    vec2 vPos = vec2(aVertexPosition);',
+            '    vec3 pos = uPMatrix * uMVMatrix * vec3(vPos, 1.0);',
+            '    gl_Position = vec4(pos, 1.0);',
+            '}',
+        ].join('\n')
+        fragment_shader_src = [
+            '#ifdef GL_ES',
+            'precision highp float;',
+            '#endif',
+            'uniform vec4 uColor;',
+            'void main(void) {',
+            '    gl_FragColor = uColor;',
+            '}',
+        ].join('\n')
+        vertexShader = this.createShader(vertex_shader_src, @gl.VERTEX_SHADER)
+        fragmentShader = this.createShader(fragment_shader_src, @gl.FRAGMENT_SHADER)
+        @shaderProgram = @gl.createProgram()
+        @gl.attachShader(@shaderProgram, vertexShader)
+        @gl.attachShader(@shaderProgram, fragmentShader)
+        @gl.linkProgram(@shaderProgram)
+        @gl.useProgram(@shaderProgram)
+        @shaderProgram.vertexPositionAttribute = @gl.getAttribLocation(@shaderProgram, "aVertexPosition")
+        @gl.enableVertexAttribArray(@shaderProgram.vertexPositionAttribute)
+        @shaderProgram.pMatrixUniform = @gl.getUniformLocation(@shaderProgram, "uPMatrix");
+        @shaderProgram.mvMatrixUniform = @gl.getUniformLocation(@shaderProgram, "uMVMatrix");
+        @shaderProgram.glColorUniform = @gl.getUniformLocation(@shaderProgram, "uColor");
+    
+    createShader: (src, type) =>
+        shader = @gl.createShader(type)
+        @gl.shaderSource(shader, src)
+        @gl.compileShader(shader)
+        return shader
+    
+    initDraw: =>
+        this.gl.clearColor(1.0, 1.0, 1.0, 1.0)
+    
+    reshapeViewport: =>
+        width = @canvas.width()
+        height = @canvas.height()
+        @gl.viewport(0, 0, width, height)
+        mat3.ortho2D(@pMatrix, width, 0, height, 0)
+        this.setPMatrixUniform()
+    
+    setColour: (r, g, b) =>
+        @gl.uniform4f(@shaderProgram.glColorUniform, r, g, b, 1.0)
+    
+    drawBox: (x, y, width, height, angle, colour) =>
+        mat3.identity(@mvMatrix)
+        mat3.translate(@mvMatrix, this.scale(x), this.scale(y))
+        mat3.rotate(@mvMatrix, angle)
+        mat3.scale(@mvMatrix, this.scale(width), this.scale(height));
+        this.setMVMatrixUniform()
+        @boxShapeVbo.bind()
+        this.setColour(0.0, 0.0, 0.0)
+        @boxShapeVbo.fill()
+    
+    drawCircle: (x, y, radius, colour) =>
+        mat3.identity(@mvMatrix)
+        mat3.translate(@mvMatrix, this.scale(x), this.scale(y))
+        mat3.scale(@mvMatrix, this.scale(radius), this.scale(radius))
+        this.setMVMatrixUniform()
+        @circleShapeVbo.bind()
+        this.setColour(0.0, 0.0, 0.0)
+        @circleShapeVbo.stroke()
+    
+    draw: =>
+        @gl.clear(@gl.COLOR_BUFFER_BIT)
+        for drawable in @parent.drawables
+            switch drawable.shape.type
+                when 'polygon'
+                    width = (Math.abs(drawable.shape.vertices[0].x) + Math.abs(drawable.shape.vertices[1].x))
+                    height = (Math.abs(drawable.shape.vertices[1].y) + Math.abs(drawable.shape.vertices[2].y))
+                    this.drawBox(
+                        drawable.position.x,
+                        drawable.position.y,
+                        width,
+                        height,
+                        drawable.angle,
+                        [0, 0, 0]
+                    )
+                when 'circle'
+                    this.drawCircle(
+                        drawable.position.x,
+                        drawable.position.y,
+                        drawable.shape.radius,
+                        [0, 0, 0]
+                    )
+
 
 jQuery ->
     window.engine = new Engine()
--- static/scripts/main.js
+++ static/scripts/main.js
@@ -12,7 +12,7 @@
     function Engine() {
       this.physicsMessage = __bind(this.physicsMessage, this);;      this.drawables = [];
       this.remotes = new Remotes(this);
-      this.viewport = new CanvasViewport(this);
+      this.viewport = new WebGLViewport(this);
       this.physics = new Worker('/static/scripts/physics.js');
       this.physics.onmessage = this.physicsMessage;
       this.physics.onerror = this.physicsError;
@@ -78,8 +78,8 @@
       this.scale = __bind(this.scale, this);;
       this.loop = __bind(this.loop, this);;
       this.canvas = $('canvas#viewport');
-      this.context = this.canvas[0].getContext("2d");
       this.viewportScale = 20;
+      this.fpsTarget = 1000 / 50;
       this.width = 0;
       this.height = 0;
       this.loopTimer = false;
@@ -88,7 +88,6 @@
       this.frames = 0;
       this.lastUpdate = 0;
       this.fps();
-      this.loop();
     }
     Viewport.prototype.loop = function() {
       var delta, now;
@@ -99,7 +98,7 @@
       this.frames++;
       return this.loopTimer = setTimeout((__bind(function() {
         return this.loop();
-      }, this)), 1000 / 50);
+      }, this)), this.fpsTarget);
     };
     Viewport.prototype.scale = function(qty) {
       return qty * this.viewportScale;
@@ -115,10 +114,14 @@
     return Viewport;
   })();
   CanvasViewport = (function() {
-    function CanvasViewport() {
-      this.draw = __bind(this.draw, this);;      CanvasViewport.__super__.constructor.apply(this, arguments);
-    }
     __extends(CanvasViewport, Viewport);
+    function CanvasViewport(parent) {
+      this.parent = parent;
+      this.draw = __bind(this.draw, this);;
+      CanvasViewport.__super__.constructor.call(this, this.parent);
+      this.context = this.canvas[0].getContext("2d");
+      this.loop();
+    }
     CanvasViewport.prototype.draw = function() {
       var drawable, height, width, _i, _len, _ref, _results;
       this.context.clearRect(0, 0, this.canvas.width(), this.canvas.height());
@@ -143,11 +146,174 @@
     return CanvasViewport;
   })();
   WebGLViewport = (function() {
-    function WebGLViewport() {
-      this.draw = __bind(this.draw, this);;      WebGLViewport.__super__.constructor.apply(this, arguments);
-    }
     __extends(WebGLViewport, Viewport);
-    WebGLViewport.prototype.draw = function() {};
+    function WebGLViewport(parent) {
+      this.parent = parent;
+      this.draw = __bind(this.draw, this);;
+      this.drawCircle = __bind(this.drawCircle, this);;
+      this.drawBox = __bind(this.drawBox, this);;
+      this.setColour = __bind(this.setColour, this);;
+      this.reshapeViewport = __bind(this.reshapeViewport, this);;
+      this.initDraw = __bind(this.initDraw, this);;
+      this.createShader = __bind(this.createShader, this);;
+      this.initShaders = __bind(this.initShaders, this);;
+      this.setMVMatrixUniform = __bind(this.setMVMatrixUniform, this);;
+      this.setPMatrixUniform = __bind(this.setPMatrixUniform, this);;
+      this.createCircleShapeVbo = __bind(this.createCircleShapeVbo, this);;
+      this.createBoxShapeVbo = __bind(this.createBoxShapeVbo, this);;
+      this.createStaticShapeVbo = __bind(this.createStaticShapeVbo, this);;
+      WebGLViewport.__super__.constructor.call(this, this.parent);
+      this.gl = WebGLUtils.create3DContext(this.canvas[0], null);
+      this.mvMatrix = mat3.create();
+      this.pMatrix = mat3.create();
+      this.circleDetail = 16;
+      this.circleEdges = 32;
+      this.circleShapeVbo = this.createCircleShapeVbo();
+      this.boxShapeVbo = this.createBoxShapeVbo();
+      this.initShaders();
+      this.initDraw();
+      this.reshapeViewport();
+      this.loop();
+    }
+    WebGLViewport.prototype.createStaticShapeVbo = function(vertices, itemSize, numItems) {
+      var vbo;
+      vbo = this.gl.createBuffer();
+      this.gl.bindBuffer(this.gl.ARRAY_BUFFER, vbo);
+      this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(vertices), this.gl.STATIC_DRAW);
+      return {
+        numItems: numItems,
+        bind: __bind(function(vertexPositionAttribute) {
+          this.gl.bindBuffer(this.gl.ARRAY_BUFFER, vbo);
+          return this.gl.vertexAttribPointer(vertexPositionAttribute, itemSize, this.gl.FLOAT, false, 0, 0);
+        }, this)
+      };
+    };
+    WebGLViewport.prototype.createBoxShapeVbo = function() {
+      var vbo, vertices;
+      vertices = [-1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, -1.0, -1.0];
+      vbo = this.createStaticShapeVbo(vertices, 2, 5);
+      return {
+        bind: vbo.bind,
+        fill: __bind(function() {
+          return this.gl.drawArrays(this.gl.TRIANGLE_FAN, 0, 4);
+        }, this),
+        stroke: __bind(function() {
+          return this.gl.drawArrays(this.gl.LINE_STRIP, 0, 5);
+        }, this)
+      };
+    };
+    WebGLViewport.prototype.createCircleShapeVbo = function() {
+      var angle, i, step, vbo, vertices, _ref;
+      vertices = [0.0, 0.0];
+      step = 2.0 * Math.PI / this.circleEdges;
+      for (i = 0, _ref = this.circleEdges; (0 <= _ref ? i < _ref : i > _ref); (0 <= _ref ? i += 1 : i -= 1)) {
+        angle = step * i;
+        vertices[i * 2 + 2] = Math.cos(angle);
+        vertices[i * 2 + 3] = Math.sin(angle);
+      }
+      vertices[this.circleEdges * 2 + 2] = vertices[2];
+      vertices[this.circleEdges * 2 + 3] = vertices[3];
+      vbo = this.createStaticShapeVbo(vertices, 2, this.circleEdges + 2);
+      return {
+        bind: vbo.bind,
+        fill: __bind(function() {
+          return this.gl.drawArrays(this.gl.TRIANGLE_FAN, 0, this.circleEdges + 2);
+        }, this),
+        stroke: __bind(function() {
+          return this.gl.drawArrays(this.gl.LINE_STRIP, 1, this.circleEdges + 1);
+        }, this),
+        destroy: __bind(function() {
+          try {
+            return this.gl.deleteBuffer(vbo);
+          } catch (error) {
+            return console.log(error);
+          }
+        }, this)
+      };
+    };
+    WebGLViewport.prototype.setPMatrixUniform = function() {
+      return this.gl.uniformMatrix3fv(this.shaderProgram.pMatrixUniform, false, this.pMatrix);
+    };
+    WebGLViewport.prototype.setMVMatrixUniform = function() {
+      return this.gl.uniformMatrix3fv(this.shaderProgram.mvMatrixUniform, false, this.mvMatrix);
+    };
+    WebGLViewport.prototype.initShaders = function() {
+      var fragmentShader, fragment_shader_src, vertexShader, vertex_shader_src;
+      vertex_shader_src = ['attribute vec3 aVertexPosition;', 'uniform mat3 uMVMatrix;', 'uniform mat3 uPMatrix;', 'void main(void) {', '    vec2 vPos = vec2(aVertexPosition);', '    vec3 pos = uPMatrix * uMVMatrix * vec3(vPos, 1.0);', '    gl_Position = vec4(pos, 1.0);', '}'].join('\n');
+      fragment_shader_src = ['#ifdef GL_ES', 'precision highp float;', '#endif', 'uniform vec4 uColor;', 'void main(void) {', '    gl_FragColor = uColor;', '}'].join('\n');
+      vertexShader = this.createShader(vertex_shader_src, this.gl.VERTEX_SHADER);
+      fragmentShader = this.createShader(fragment_shader_src, this.gl.FRAGMENT_SHADER);
+      this.shaderProgram = this.gl.createProgram();
+      this.gl.attachShader(this.shaderProgram, vertexShader);
+      this.gl.attachShader(this.shaderProgram, fragmentShader);
+      this.gl.linkProgram(this.shaderProgram);
+      this.gl.useProgram(this.shaderProgram);
+      this.shaderProgram.vertexPositionAttribute = this.gl.getAttribLocation(this.shaderProgram, "aVertexPosition");
+      this.gl.enableVertexAttribArray(this.shaderProgram.vertexPositionAttribute);
+      this.shaderProgram.pMatrixUniform = this.gl.getUniformLocation(this.shaderProgram, "uPMatrix");
+      this.shaderProgram.mvMatrixUniform = this.gl.getUniformLocation(this.shaderProgram, "uMVMatrix");
+      return this.shaderProgram.glColorUniform = this.gl.getUniformLocation(this.shaderProgram, "uColor");
+    };
+    WebGLViewport.prototype.createShader = function(src, type) {
+      var shader;
+      shader = this.gl.createShader(type);
+      this.gl.shaderSource(shader, src);
+      this.gl.compileShader(shader);
+      return shader;
+    };
+    WebGLViewport.prototype.initDraw = function() {
+      return this.gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    };
+    WebGLViewport.prototype.reshapeViewport = function() {
+      var height, width;
+      width = this.canvas.width();
+      height = this.canvas.height();
+      this.gl.viewport(0, 0, width, height);
+      mat3.ortho2D(this.pMatrix, width, 0, height, 0);
+      return this.setPMatrixUniform();
+    };
+    WebGLViewport.prototype.setColour = function(r, g, b) {
+      return this.gl.uniform4f(this.shaderProgram.glColorUniform, r, g, b, 1.0);
+    };
+    WebGLViewport.prototype.drawBox = function(x, y, width, height, angle, colour) {
+      mat3.identity(this.mvMatrix);
+      mat3.translate(this.mvMatrix, this.scale(x), this.scale(y));
+      mat3.rotate(this.mvMatrix, angle);
+      mat3.scale(this.mvMatrix, this.scale(width), this.scale(height));
+      this.setMVMatrixUniform();
+      this.boxShapeVbo.bind();
+      this.setColour(0.0, 0.0, 0.0);
+      return this.boxShapeVbo.fill();
+    };
+    WebGLViewport.prototype.drawCircle = function(x, y, radius, colour) {
+      mat3.identity(this.mvMatrix);
+      mat3.translate(this.mvMatrix, this.scale(x), this.scale(y));
+      mat3.scale(this.mvMatrix, this.scale(radius), this.scale(radius));
+      this.setMVMatrixUniform();
+      this.circleShapeVbo.bind();
+      this.setColour(0.0, 0.0, 0.0);
+      return this.circleShapeVbo.stroke();
+    };
+    WebGLViewport.prototype.draw = function() {
+      var drawable, height, width, _i, _len, _ref, _results;
+      this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+      _ref = this.parent.drawables;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        drawable = _ref[_i];
+        _results.push((function() {
+          switch (drawable.shape.type) {
+            case 'polygon':
+              width = Math.abs(drawable.shape.vertices[0].x) + Math.abs(drawable.shape.vertices[1].x);
+              height = Math.abs(drawable.shape.vertices[1].y) + Math.abs(drawable.shape.vertices[2].y);
+              return this.drawBox(drawable.position.x, drawable.position.y, width, height, drawable.angle, [0, 0, 0]);
+            case 'circle':
+              return this.drawCircle(drawable.position.x, drawable.position.y, drawable.shape.radius, [0, 0, 0]);
+          }
+        }).call(this));
+      }
+      return _results;
+    };
     return WebGLViewport;
   })();
   jQuery(function() {
